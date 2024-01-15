9fcf469091896c50f1f9eff9dce69a334d127b01
Kavya Malviya <122686888+KavyaMalviya56@users.noreply.github.com>
2024-01-15 21:56:52 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:gossipy/repositories/message_repository.dart';
+import 'package:gossipy/services/api_client.dart';
+import 'package:gossipy/services/websocket_client.dart';
+import 'package:models/models.dart';
+
+import 'screens/chat_room_screen.dart';
+
+final apiClient = ApiClient(tokenProvider: () async {
+  //TODO : Get the bearer token of the current user
+  return '';
+});
+
+final webSocketClient = WebSocketClient();
+final MessageRepository messageRepository =
+    MessageRepository(apiClient: apiClient, webSocketClient: webSocketClient);
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: ChatRoomScreen(chatRoom: chatRoom));
+  }
+}
+
+const userId1 = 'e7b76f42-c291-4ff3-8115-4d7fe93e2f0c';
+const userId2 = '65f16c13-4698-46d8-b343-b27e31427dce';
+
+final chatRoom = ChatRoom(
+  id: '8d162274-6cb8-4776-815a-8e721ebfb76d',
+  participants: const [
+    User(
+      id: userId1,
+      username: 'User 1',
+      phone: '1234512345',
+      email: 'user1@email.com',
+      avatarUrl:
+          'https://images.unsplash.com/photo-1700493624764-f7524969037d?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      status: 'online',
+    ),
+    User(
+      id: userId2,
+      username: 'User 2',
+      phone: '5432154321',
+      email: 'user2@email.com',
+      avatarUrl:
+          'https://images.unsplash.com/photo-1700469880511-3ef0cee47985?q=80&w=3672&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      status: 'online',
+    ),
+  ],
+  lastMessage: Message(
+    id: 'de120f3a-dbca-4330-9e2e-18b55a2fb9e5',
+    chatRoomId: '8d162274-6cb8-4776-815a-8e721ebfb76d',
+    senderUserId: userId1,
+    receiverUserId: userId2,
+    content: 'Hey! I am good, thanks.',
+    createdAt: DateTime(2023, 12, 1, 1, 0, 0),
+  ),
+  unreadCount: 0,
+);
