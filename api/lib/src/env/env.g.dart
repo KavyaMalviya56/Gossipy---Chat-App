cd8639acfb8152f3add999b0efb8e284f78f4e0f
Kavya Malviya <122686888+KavyaMalviya56@users.noreply.github.com>
2024-01-15 21:58:45 +0530

    Add files via upload

---
+++ api/lib/src/env/env.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _Env {
+  static const String SUPABASE_URL = 'https://cnjzdrdaarkrltlalzhq.supabase.co';
+
+  static const String SUPABASE_ANON_KEY =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuanpkcmRhYXJrcmx0bGFsemhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ3NzkxMDEsImV4cCI6MjAyMDM1NTEwMX0.MGOll9vzUeLoeE4yspgZiVpStTn1O9iNnYbVsZzjkOQ';
+
+  static const String SUPABASE_SERVICE_ROLE_KEY =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuanpkcmRhYXJrcmx0bGFsemhxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNDc3OTEwMSwiZXhwIjoyMDIwMzU1MTAxfQ.jQu0Ewc38vruPR0obXqr3U-CXaIJCGHknQsM2C0osxA';
+}
